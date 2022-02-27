3b940ade2f6c3af9c03f0d0ae2b949aab59fed00
Cyrex2142 <93499531+Cyrex2142@users.noreply.github.com>
2022-02-27 18:33:59 +1030

    Update BinDec_Converter_by_Cyrex.py
    adding commenting for the sake of it idk what Im doing lmao

--- BinDec_Converter_by_Cyrex.py
+++ BinDec_Converter_by_Cyrex.py
@@ -3,30 +3,29 @@ decimal_result = 0
 decNumSave = 0
 #function to output author details
 def display_details():
-    print("File     : Allby005_inbetween.py")
-    print("Author   : Bradley Allison")
-    print("Stud ID  : 110130836")
-    print("Email ID : allby005")
+    print("File     : BinDec_converter.py")
+    print("Author   : Cyrex")
+    print("Email ID : null")
     print("This is my own work as defined by the University's Academic Misconduct Policy.")
     
 #decimal to binary conversion.
 def convert_to_binary(decimal_number):
-    #variable list to stare modulo values.
+    #variable list to stare modulated values.
     calcNum = []
-    #check that decimal number is greater than zero and divisible.
+    #check that decimal number is greater than zero.
     while decimal_number != 0:
         #divide decimal by 2 to step through conversion.
         quotient = int(decimal_number / 2)
-        #append the remainder (1 or 0) to the calculation list.
+        #append the remainder (1 or 0) to the calculation list using the '%' modulo calculator.
         calcNum.append(int(decimal_number % 2))
         #update the decimal with the next conversion step.
         decimal_number = int(quotient)
-    #set up variables for reversing the calculated remainders to print accurate binary.    
+    #set up variables for reversing the calculated remainders to print accurate binary, the intital output needs to be reversed for display and converting back against a 2** table as seen below.    
     binLength = len(calcNum)
-    #set up list for final output number and index to reverse entries.
+    #set up list for final output number and swap variable for indexing to reverse entries.
     swap = binLength
     binNum =[]
-    #for loop to swap the orer of the binary signals.
+    #for loop to swap the order of the binary signals.
     for val in calcNum:
         binNum.append(val)
     for val in calcNum:
@@ -37,18 +36,25 @@ def convert_to_binary(decimal_number):
     binPrint = ''
     for x in binNum:
         binPrint += str(x)
-
+    #Return the final string value for console output.
     return binPrint
 
-
+#Function to convert from binary to decimal.
 def convert_to_decimal(binary_number):
+    #This should probably be placed outside the function but hey it was Uni and I'm not pre-reptilian Zuccerberg.
     binary_number = str(binary_number)
+    #calling global val here for some weird stability issue I wasnt sure of at the time...
     global decimal_result
+    #setting list variable to store the binary string input as individual integer values.
     binary_digits = []
+    #binary conversion table, can be extended as long as required but this was as far as need to be, looking back an integer variable would probably have been more suitable and incremented on each itteraion...will update soon.
     decimal_conversion_list = [2**0, 2**1, 2**2, 2**3, 2**4, 2**5, 2**6, 2**7, 2**8]
+    #index count var
     index = 0
+    #for loop to build a list on integers from the user binary string i.e. '1010' to [1, 0, 1, 0]
     for x in binary_number:
         binary_digits.append(int(x))
+    #for loop to test if binary indication is 'On' and weight that indication against the corresponding conversion value, i.e. [1, 0, 1] = (1*2**0) + 0 + (1*2**2) = 5
     for val in binary_digits:
         if val == 1:
             decimal_result = decimal_result + decimal_conversion_list[index]    
